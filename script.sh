479e5997d3092e42f4d0ab8fcc5014fec04b2d3a
BOrekhova <barbaraorekhova@gmail.com>
2024-06-23 17:51:31 +0000

    !

--- script.sh
+++ script.sh
@@ -8,13 +8,14 @@ foo () {
   ln -s $DOTS_DIR/$1 $USER_DIR/.config
 }
 
-foo "berry"
+# foo "berry"
 foo "fish"
 foo "helix"
 foo "kitty"
+foo "herbstluftwm"
 
 # ln -sf $DOTS_DIR/vscode/settings.json $USER_DIR/.config/Code/User/
-ln -sf $DOTS_DIR/x11/.xinitrc $USER_DIR/
+ln -sf $DOTS_DIR/x11/xinitrc $USER_DIR/.xinitrc
 
 cp $DOTS_DIR/x11/30-touchpad.conf /etc/X11/xorg.conf.d/
 
